d63fe0b5905b0367577f63815ff431c0ab434bb6
esmond-adjei <xmongram@gmail.com>
2024-01-01 23:02:04 +0000

    modified some files

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -47,7 +47,8 @@ class Server:
             self.dataset()
         return self.__dataset[start_index:end_index]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10):
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> \
+            Dict[str, Union[Union[int, None], List[List]]]:
         """
         Gets items for a given page and page_size with hypermedia
         for next_page, and prev_page
